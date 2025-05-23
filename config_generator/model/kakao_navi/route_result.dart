b207c3717534cff772f22b536343a41395657334
gunyu1019 <gunyu1019@gmail.com>
2025-05-23 15:03:18 +0000

    [Feat] Add route result model for kakao navi route API.

---
+++ config_generator/model/kakao_navi/route_result.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'route.dart';
+
+class RouteResult {
+  final String transId;
+  final List<Route> routes;
+
+  RouteResult(this.transId, this.routes);
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'transId': transId,
+      'routes': routes.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory RouteResult.fromMap(Map<String, dynamic> map) {
+    return RouteResult(
+      map['transId'] as String,
+      List<Route>.from((map['routes'] as List<int>).map<Route>((x) => Route.fromMap(x as Map<String,dynamic>),),),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory RouteResult.fromJson(String source) => RouteResult.fromMap(json.decode(source) as Map<String, dynamic>);
+}
