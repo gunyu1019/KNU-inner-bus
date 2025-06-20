3a1e2c960ceaf756c5077d87d30f0a6a0ed0d1b8
gunyu1019 <gunyu1019@gmail.com>
2025-06-20 15:03:04 +0000

    [Feat] Add Station Detail Page.

---
+++ lib/screen/page/station_detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StationDetail extends StatefulWidget {
+  const StationDetail({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
