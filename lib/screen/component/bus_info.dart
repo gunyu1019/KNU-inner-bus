23f1f7e743a77e84b964339fec06437bb7246009
gunyu1019 <gunyu1019@gmail.com>
2025-06-20 15:09:01 +0000

    [Docs] Docstring Bus Info.

--- lib/screen/component/bus_info.dart
+++ lib/screen/component/bus_info.dart
@@ -3,6 +3,7 @@ import 'package:knu_inner_bus/constant/color.dart';
 import 'package:knu_inner_bus/constant/text_style.dart';
 import 'package:knu_inner_bus/model/bus_state.dart';
 
+/// 버스 시간표를 구현하는 위젯입니다.
 class BusInfo extends StatelessWidget {
   const BusInfo({
     super.key,
@@ -15,15 +16,38 @@ class BusInfo extends StatelessWidget {
     this.descriptionFomrat = '{MM}분 후 도착 예정 {CURRENT_STATION}',
   });
 
+  /// 버스의 운행 회차를 의미합니다.
   final int index;
+
+  /// 버스의 출발ㆍ도착 시간 입니다.
   final DateTime dateTime;
+
+  /// 설명글(Descriotion)을 제외할 지 여부입니다.
+  /// 
+  /// 설명글은 현재 시간과 비교하여 남은 시간을 표시하며, [StationSummary]에서 사용됩니다.
+  /// 예시: "5분 후 도착 예정 중앙도서관"
   final bool simple;
+
+  /// 마지막 회차인지 여부입니다.
   final bool isLast;
+
+  /// 버스의 현재 상태입니다.
+  /// - [BusState.closed]: 운행 종료
+  /// - [BusState.current]: 이번 배차에 도착할 버스
+  /// - [BusState.previous]: 이미 지나간 회차
+  /// - [BusState.next]: 다음 배차에 도착할 버스
   final BusState busState;
+
+  /// 설명글의 포맷입니다.
+  /// 기본값은 '{MM}분 후 도착 예정 {CURRENT_STATION}' 입니다.
   final String descriptionFomrat;
 
+  /// 현재 버스가 위치한 정류장 이름입니다.
   final String? currentStation;
 
+  /// 위젯을 구성요소 중 하나입니다.
+  /// [BusState.closed], [BusState.current] 에서 사용됩니다.
+  /// [color] 매개변수로 테두리의 색상을 지정할 수 있습니다.
   Widget bigRing(Color color) => Container(
     width: 24,
     height: 24,
@@ -40,6 +64,9 @@ class BusInfo extends StatelessWidget {
     child: const SizedBox.shrink(),
   );
 
+  /// 위젯을 구성요소 중 하나입니다.
+  /// [BusState.previous], [BusState.next] 에서 사용됩니다.
+  /// [color] 매개변수로 원형의 색상을 지정할 수 있습니다.
   Widget smallRing(Color color) => Container(
     width: 20,
     height: 20,
@@ -47,6 +74,7 @@ class BusInfo extends StatelessWidget {
     child: const SizedBox.shrink(),
   );
 
+  /// 현재 버스의 상태에 따라 원형을 불러옵니다.
   Widget hightlightRing() => switch (busState) {
     BusState.closed => bigRing(ThemeColor.closedTextColor),
     BusState.current => bigRing(ThemeColor.hightlightTextColor),
@@ -54,6 +82,7 @@ class BusInfo extends StatelessWidget {
     BusState.next => smallRing(ThemeColor.black),
   };
 
+  /// 버스 상태에 따른 위젯의 패딩을 반환합니다.
   EdgeInsets get padding => switch (busState) {
     BusState.closed => EdgeInsets.symmetric(horizontal: 18, vertical: 12),
     BusState.current => EdgeInsets.all(18),
@@ -61,6 +90,7 @@ class BusInfo extends StatelessWidget {
     BusState.next => EdgeInsets.fromLTRB(20, 8, 16, 8),
   };
 
+  /// 버스의 회차를 표시하는 텍스트 위젯입니다.
   Text countText() =>
       isLast
           ? Text('마지막 회차', style: KakaoMapTextStyle.finalCount)
@@ -74,6 +104,8 @@ class BusInfo extends StatelessWidget {
             },
           );
 
+  /// 현재 버스의 상태에 따라 강조 표시 여부를 반환합니다.
+  /// [BusState.closed] 또는 [BusState.current] 상태일 때 강조 표시됩니다.
   bool get isHightlight =>
       [BusState.closed, BusState.current].contains(busState);
 
