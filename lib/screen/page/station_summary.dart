8245254a75d52a35f6b0a783c6214a9b477bd6e2
gunyu1019 <gunyu1019@gmail.com>
2025-06-20 15:14:41 +0000

    [Docs] Docstring Station Summary.

--- lib/screen/page/station_summary.dart
+++ lib/screen/page/station_summary.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:knu_inner_bus/model/route_path.dart';
 import 'package:knu_inner_bus/screen/component/station_summary_item.dart';
 
+/// 정류장 요약 정보를 표시하는 페이지입니다.
+/// [StationSummaryItem]을 종합하여 노선에 있는 정류장 정보를 나타냅니다.
+/// 이 페이지는 모바일을 위해 구성되었으며, 태블릿, PC 등의 환경에서 사용하지 않습니다.
 class StationSummary extends StatefulWidget {
   const StationSummary({super.key, required this.size, required this.route});
 
+  /// 하나의 페이지를 구성하는 크기입니다.
+  /// 가로 길이는 디바이스 너비를 기준으로 하며,
+  /// 세로 길이는 300px로 고정하는 것을 권장합니다.
   final Size size;
+
+  /// 노선에 있는 정류장 정보를 담고 있는 객체입니다.
   final RoutePath route;
 
   @override
@@ -14,13 +22,19 @@ class StationSummary extends StatefulWidget {
 
 class StationSummaryPager extends State<StationSummary> {
   late final ScrollController _scrollController;
+
+  /// 페이지 크기입니다.
   late final double pageSize;
   late int _currentPage;
   late double _currentOffset;
 
+  /// 현재 페이지를 나타내는 변수입니다.
   int get currentPage => _currentPage;
+
+  /// 현재 페이지의 오프셋을 나타내는 변수입니다.
   double get currentOffset => _currentOffset;
 
+  /// [page] 매개변수에 따라 애니메이션을 적용한 상태로 스크롤합니다.
   Future<void> scrollToPage(int page) async {
     if (page < 0 || page >= widget.route.station.length) {
       return;
@@ -34,6 +48,7 @@ class StationSummaryPager extends State<StationSummary> {
     );
   }
 
+  /// [page] 매개변수에 따라 애니메이션 없이 이동합니다.
   void jumpToPage(int page) {
     if (page < 0 || page >= widget.route.station.length) {
       return;
@@ -52,6 +67,8 @@ class StationSummaryPager extends State<StationSummary> {
     _currentOffset = pageSize;
   }
 
+  /// 방향을 눌렀을 때 호출되는 함수입니다.
+  /// 현재 페이지의 정류장 정보를 기준으로 다른 방향에 있는 정류장으로 이동합니다.
   void _directionTap() {
     final currentStation = widget.route.station[currentPage - 1];
 
@@ -77,6 +94,9 @@ class StationSummaryPager extends State<StationSummary> {
     }
   }
 
+  /// ListView의 아이템을 생성하는 함수입니다.
+  /// [StationSummary]에 구성된 ListView는 무한히 스크롤 할 수 있도록 구성합니다.
+  /// 따라서 첫 번째 아이템은 마지막 정류장 정보를, 마지막 아이템은 첫 번째 정류장 정보를 표시합니다.
   Widget buildItem(BuildContext context, int index) {
     final int realIndex =
         index == 0
