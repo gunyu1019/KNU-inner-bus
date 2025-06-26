62416dc9e638881f3597a8bde286d55755d76c48
gunyu1019 <gunyu1019@gmail.com>
2025-06-26 13:23:04 +0000

    [Feat] #3 - Apply title on station detail.

--- lib/screen/page/station_detail.dart
+++ lib/screen/page/station_detail.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:knu_inner_bus/model/station.dart';
+import 'package:knu_inner_bus/screen/component/station_detail_title.dart';
 
 class StationDetail extends StatelessWidget {
-  const StationDetail({super.key});
+  const StationDetail({super.key, required this.size, required this.station});
+
+  /// 하나의 페이지를 구성하는 크기입니다.
+  final Size size;
+
+  final Station station;
   
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: [],),
+    return SizedBox(
+      width: size.width,
+      height: size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          StationDetailTitle(
+            station: station, 
+            previousName: "이전 정류장", 
+            nextName: "다음 정류장",
+            size: size,
+          )
+        ],
+      ),
     );
   }
 }
